23dbf3bc5e42aea04b233bb30a17fe0d2a5a81c0
Nikita <nikich340@gmail.com>
2015-10-29 14:36:29 +1000

    Fixes for odex

--- prebuilt/fixup.sh
+++ prebuilt/fixup.sh
@@ -3,8 +3,9 @@
 model=`cat /proc/cmdline | sed 's/.*model.name=\([^ ]*\).*/\1/'`
 
 if [ "$model" = "LG-D618" ] || [ "$model" = "LG-D610" ]; then
-	rm /system/priv-app/TeleService/TeleService.apk
-	mv /system/nonLTE/TeleService/TeleService.apk /system/priv-app/TeleService/TeleService.apk
+	rm -rf /system/priv-app/TeleService
+	mkdir /system/priv-app/TeleService
+	mv /system/nonLTE/TeleService/* /system/priv-app/TeleService
 	rm /system/etc/permissions/android.hardware.nfc.xml
 	rm /system/etc/permissions/android.hardware.nfc.hce.xml
 	rm /system/lib/hw/nfc_nci.pn54x.default.so
