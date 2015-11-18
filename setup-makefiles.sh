90cda092259cf8b15918ebba218d45192cf6095c
Nikita <nikich340@gmail.com>
2015-11-18 17:26:54 +1000

    Add chromium monster to prebuilt again It will reduce build time twice...

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -65,11 +65,11 @@ PRODUCT_PACKAGES += \\
     com.qualcomm.location \\
     qcrilmsgtunnel \\
     shutdownlistener \\
-    webview \\
-    LGCalculator
+    webview
 
 PRODUCT_PACKAGES += \\
-    libtime_genoff
+    libtime_genoff \\
+    libwebviewchromium
 
 PRODUCT_PACKAGES += \\
     libscale \\
@@ -158,10 +158,30 @@ LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
+LOCAL_MODULE := webview
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/app/webview/webview.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := $VENDOR
 LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
 LOCAL_MODULE_PATH := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libwebviewchromium
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES := proprietary/lib/libwebviewchromium.so
+LOCAL_MODULE_PATH := \$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
